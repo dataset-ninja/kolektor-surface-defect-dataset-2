f3fb4d632ee44a06367ad246cd0c31d95669118a
Arsenii Vladimirov <78355358+grokhi@users.noreply.github.com>
2023-08-11 11:50:41 +0600

    Update settings.py

--- src/settings.py
+++ src/settings.py
@@ -41,7 +41,7 @@ if RELEASE_DATE is None:
 HOMEPAGE_URL: str = "https://www.vicos.si/resources/kolektorsdd2/"
 # e.g. "https://some.com/dataset/homepage"
 
-PREVIEW_IMAGE_ID: int = 220990
+PREVIEW_IMAGE_ID: int = 220881
 # This should be filled AFTER uploading images to instance, just ID of any image.
 
 GITHUB_URL: str = "https://github.com/dataset-ninja/kolektor-surface-defect-dataset-2"

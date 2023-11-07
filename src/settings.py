3c38e4d1e1628fbe919ce7c47d07b78563a6053c
Arsenii Vladimirov <78355358+grokhi@users.noreply.github.com>
2023-11-07 23:48:58 +0600

    Update settings.py

--- src/settings.py
+++ src/settings.py
@@ -14,7 +14,7 @@ from dataset_tools.templates import (
 # * Before uploading to instance #
 ##################################
 PROJECT_NAME: str = "KolektorSDD2"
-PROJECT_NAME_FULL: str = "Kolektor Surface-Defect Dataset 2"
+PROJECT_NAME_FULL: str = "KolektorSDD2: Kolektor Surface-Defect Dataset 2"
 HIDE_DATASET = False  # set False when 100% sure about repo quality
 
 ##################################
